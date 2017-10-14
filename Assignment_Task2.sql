8350739497f248e0e1bd4bc1371d079f3103ad7e
theterrificman <j28.choi@connect.qut.edu.au>
2017-10-15 09:26:12 +1000

    Add files via upload
    final

--- Assignment_Task2.sql
+++ Assignment_Task2.sql
@@ -1,3 +1,8 @@
+/* Ji Su Choi - n9678166 */
+/* Min Ho Kim - n8051381   */
+/* Task2, Task3, Task4, Task5 */
+
+/* Task 2 */
 /*Task2_question1*/
 SELECT firstName, lastName, dob, gender, email 
 FROM player
@@ -15,33 +20,40 @@ FROM Phonenumber
 WHERE username = ( SELECT username from Player ORDER BY dob ASC LIMIT 1);
 
 /*Task2_question4*/
-SELECT badge.badgeName, player.firstName, player.lastName, player.email
-FROM player, badge, purchase
-WHERE player.username = purchase.username AND
-badge.badgeID = purchase.badgeID
-ORDER BY badge.badgeName,player.firstName,player.lastName ASC;
+SELECT b.badgeName, p.firstName, p.lastName, p.email
+FROM player p, badge b, purchase pc
+WHERE p.username = pc.username AND
+b.badgeID = pc.badgeID
+ORDER BY b.badgeName, p.firstName, p.lastName ASC;
 
 /*Task2_question5*/
-SELECT username, COUNT(*) AS theNumberOfCompletedCommonQeust
-FROM playerprogress p, treasure t 
-WHERE  p.progress = 'complete' AND t.type = 'common'
-GROUP BY p.username;
-
-
+SELECT p.username, COUNT(*) AS theNumberOfCompletedCommonQuests
+FROM player p, playertreasure pt, treasure t, quest qu
+WHERE p.username = pt.username
+AND t.treasureID = pt.treasureID AND t.type = 'common'
+AND t.questID = qu.questID
+GROUP BY p.username; 
+                         
+/* Task 3 */
 /*Task3_question1*/
-INSERT INTO badge (badgeID, badgeName, badgeDescription)
-VALUE ('18','Fools Gold','Trickiest trickster in all the seas');
+INSERT INTO badge (badgeName, badgeDescription)
+VALUE ('Fools Gold','Trickiest trickster in all the seas');
 
 /*Task3_question2*/
-
+set sql_safe_updates = 0;
 DELETE FROM playerprogress
 WHERE progress = 'inactive';
+set sql_safe_updates = 1;
 
 /*Task3_question3*/
+set sql_safe_updates = 0;
 UPDATE player
 SET streetNumber = '72', streetName = 'Evergreen Terrace', suburb= 'Springfield'
-WHERE lastName = 'Smith';
+WHERE lastName = 'Smith' AND streetNumber = '180'
+AND streetName = 'Zelda Street' AND suburb = 'Linkburb';
+set sql_safe_updates = 1;
 
+/* Task 4 */
 /*Task4_Index*/
 CREATE INDEX treasureWebpage
 ON treasure(webpage);
@@ -50,9 +62,42 @@ ON treasure(webpage);
 CREATE VIEW newList AS 
 SELECT DISTINCT firstName, lastName, creationDateTime
 FROM player p, playerprogress prg
-WHERE p.username = prg.username AND prg.progress NOT IN ('complete');
+WHERE p.username = prg.username 
+AND prg.progress NOT IN ('complete');
+
+/* Task 5 */
+CREATE USER IF NOT EXISTS 'nikki'@'localhost' IDENTIFIED BY 'password';
+CREATE USER IF NOT EXISTS 'phil'@'localhost' IDENTIFIED BY 'password';
 
 /*Task5_A*/
+GRANT INSERT 
+ON Player
+TO 'nikki'@'localhost';
+
 /*Task5_B*/
+GRANT DELETE
+ON Player
+TO 'nikki'@'localhost';
+
+/* Give Phil Privileges */
+GRANT INSERT 
+ON Player
+TO 'phil'@'localhost';
+GRANT DELETE
+ON Player
+TO 'phil'@'localhost';
+
+
 /*Task5_C*/
+REVOKE INSERT 
+ON Player
+FROM 'phil'@'localhost';
+
 /*Task5_D*/
+REVOKE DELETE 
+ON Player
+FROM 'phil'@'localhost';
+
+
+
+
---
+++ README.txt
@@ -0,0 +1,45 @@
+Name:Ji Su Choi
+Student ID:n9678166
+
+Name: Min Ho Kim
+Student ID:n8051381
+
+Please indicate for each question/task whether it is attempted/works/not attempted. 
+If you have attempted it but it doesn't run successfully select 'attempted', if it runs then select 'works', otherwise select 'not attempted.
+For Task 5 select either attempted/not attempted.
+
+--------
+Task 1
+--------
+Create Database:'works'
+Create Tables:'works'
+
+--------
+Task 2
+--------
+Q1. 'works' 
+Q2. 'works'
+Q3. 'works'
+Q4  'works'
+Q5. 'works'
+
+--------
+Task 3
+--------
+Insert: 'works'
+Delete: 'works'
+Update: 'works'
+
+--------
+Task 4
+--------
+Create Index: 'works'
+Create View: 'works'
+
+--------
+Task 5 - Attempted
+--------
+A.Attempted
+B.Attempted
+C.Attempted
+D.Attempted
